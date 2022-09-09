bbdfe5129ab16c859660c43b160478eff8e5989c
Co-Pierre Georg <cogeorg@protonmail.com>
2022-09-09 22:19:22 +0200

    Creating random samples
    Using the last argument in 30_create_dependency_graph.py. It's not the ideal place to do the sampling, but it can save time to do the network analysis and the sample creation at the same time instead of twice.

--- 01_prepare_data.sh
+++ 01_prepare_data.sh
@@ -16,40 +16,40 @@ DEPFILE=dependencies_$BASENAME.csv
 # FIRST:
 # Download raw data from: https://zenodo.org/record/2536573/files/Libraries.io-open-data-1.4.0.tar.gz
 # THEN:
-./10_prepare_dependencies.py \
-  $BASEDIR \
-  dependencies-1.4.0-2018-12-22.csv \
-  $DEPFILE
+# ./10_prepare_dependencies.py \
+#   $BASEDIR \
+#   dependencies-1.4.0-2018-12-22.csv \
+#   $DEPFILE
 
-./11_prepare_projects.py \
-  $BASEDIR/ \
-  projects-1.4.0-2018-12-22.csv \
-  projects_npm.csv
+# ./11_prepare_projects.py \
+#   $BASEDIR/ \
+#   projects-1.4.0-2018-12-22.csv \
+#   projects_npm.csv
 
-./12_prepare_versions.py \
-  $BASEDIR \
-  versions-1.4.0-2018-12-22.csv \
-  versions_npm-restricted.csv \
-  2011-01-01 \
-  2021-12-31
+# ./12_prepare_versions.py \
+#   $BASEDIR \
+#   versions-1.4.0-2018-12-22.csv \
+#   versions_npm-restricted.csv \
+#   2011-01-01 \
+#   2021-12-31
 
 #
 # STEP 2 - CREATE DEPENDENCY GRAPH
 #
 # execute in data directory...
-cd $BASEDIR/ ; split -l 100000 -d -a 5 $DEPFILE  ; mv x* dependencies/ ; cd dependencies ; for i in `ls` ; do mv $i $i.csv ; done ; cd $GITDIR
+# cd $BASEDIR/ ; split -l 100000 -d -a 5 $DEPFILE  ; mv x* dependencies/ ; cd dependencies ; for i in `ls` ; do mv $i $i.csv ; done ; cd $GITDIR
 
 # ...then execute in this directory:
-./20_merge_data.py \
-  $BASEDIR/ \
-  dependencies/ \
-  dependencies_restricted/ \
-  $BASENAME
+# ./20_merge_data.py \
+#   $BASEDIR/ \
+#   dependencies/ \
+#   dependencies_restricted/ \
+#   $BASENAME
 
-cd $BASEDIR/dependencies_restricted/ ; \
-    rm ../dependencies_npm-merged.csv 2>/dev/null ; \
-    cat *.csv | grep -v "Project ID,Pro" >> ../dependencies_npm-merged.csv
-cd $GITDIR
+# cd $BASEDIR/dependencies_restricted/ ; \
+#     rm ../dependencies_npm-merged.csv 2>/dev/null ; \
+#     cat *.csv | grep -v "Project ID,Pro" >> ../dependencies_npm-merged.csv
+# cd $GITDIR
 
 # the last argument indicates whether or not a sample is created.
 ./30_create_dependency_graph.py \
@@ -57,4 +57,4 @@ cd $GITDIR
   dependencies_npm-merged.csv \
   dependencies_npm-merged \
   versions_npm-restricted.csv \
-  0.0
+  0.01
