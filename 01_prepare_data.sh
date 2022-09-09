282cc8707d51ff98182052ee8191d6bee3b1f76e
Co-Pierre Georg <cogeorg@protonmail.com>
2022-09-09 22:17:33 +0200

    First production run completed and stable
    I made a mess in the previous commit since it was supposed to go to v0.1 branch instead of main. Fixing it here so that this branch includes the first full production run.

--- 01_prepare_data.sh
+++ 01_prepare_data.sh
@@ -1,26 +1,45 @@
 #!/usr/bin/bash
-GITDIR=/c/Users/user-pc/git/SoftwareNetworks
-BASEDIR=/c/Users/user-pc/Dropbox/Papers/10_WorkInProgress/SoftwareNetworks/Data/test1/
-BASENAME=test1
+GITDIR=/c/Users/user-pc/git/SoftwareNetworks/
+BASENAME=npm
+BASEDIR=/c/Users/user-pc/Dropbox/Papers/10_WorkInProgress/SoftwareNetworks/Data/$BASENAME/
 DEPFILE=dependencies_$BASENAME.csv
 
 # ###########################################################################
 #
-# TEST RUN
+# PRODUCTION RUN
 #
 # ###########################################################################
 
 #
-# STEP 1 -- NOT NECESSARY SINCE INPUT FILES ARE MANUALLY PREPARED
+# STEP 1 -- PREPARE ORIGINAL DATA
 #
+# FIRST:
+# Download raw data from: https://zenodo.org/record/2536573/files/Libraries.io-open-data-1.4.0.tar.gz
+# THEN:
+./10_prepare_dependencies.py \
+  $BASEDIR \
+  dependencies-1.4.0-2018-12-22.csv \
+  $DEPFILE
+
+./11_prepare_projects.py \
+  $BASEDIR/ \
+  projects-1.4.0-2018-12-22.csv \
+  projects_npm.csv
+
+./12_prepare_versions.py \
+  $BASEDIR \
+  versions-1.4.0-2018-12-22.csv \
+  versions_npm-restricted.csv \
+  2011-01-01 \
+  2021-12-31
 
 #
-# STEP 2
+# STEP 2 - CREATE DEPENDENCY GRAPH
 #
 # execute in data directory...
-cd $BASEDIR ; split -l 100000 -d -a 5 $DEPFILE  ; mv x* dependencies/ ; cd dependencies ; for i in `ls` ; do mv $i $i.csv ; done ; cd $GITDIR
+cd $BASEDIR/ ; split -l 100000 -d -a 5 $DEPFILE  ; mv x* dependencies/ ; cd dependencies ; for i in `ls` ; do mv $i $i.csv ; done ; cd $GITDIR
 
-# ...then execute in git directory:
+# ...then execute in this directory:
 ./20_merge_data.py \
   $BASEDIR/ \
   dependencies/ \
@@ -28,13 +47,14 @@ cd $BASEDIR ; split -l 100000 -d -a 5 $DEPFILE  ; mv x* dependencies/ ; cd depen
   $BASENAME
 
 cd $BASEDIR/dependencies_restricted/ ; \
-    rm ../dependencies_$BASENAME-merged.csv 2>/dev/null ; \
-    cat *.csv | grep -v "Project ID,Pro" >> ../dependencies_$BASENAME-merged.csv ; \
-    cd $GITDIR
+    rm ../dependencies_npm-merged.csv 2>/dev/null ; \
+    cat *.csv | grep -v "Project ID,Pro" >> ../dependencies_npm-merged.csv
+cd $GITDIR
 
+# the last argument indicates whether or not a sample is created.
 ./30_create_dependency_graph.py \
-  $BASEDIR/ \
-  dependencies_$BASENAME-merged.csv \
-  dependencies_$BASENAME-merged.gexf \
-  versions_$BASENAME-restricted.csv \
-  0
+  $BASEDIR \
+  dependencies_npm-merged.csv \
+  dependencies_npm-merged \
+  versions_npm-restricted.csv \
+  0.0
